ffaa95f86b9a8a7b3e5384e9d38876c2bf2ae943
Nhu Luong <nhuluong@wireless-169-228-116-146.ucsd.edu>
2020-01-23 10:36:51 -0800

    Final commit?

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	$(".project").click(projectClick);
 })
 
 /*
@@ -11,8 +12,30 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e)	{
+	console.log("Project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+
+	var containingProject = $(this).closest(".project");
+    
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+    
+    $("myelement").fadeIn()
+    $("myelement").fadeOut()
+    $("myelement").fadeToggle()
 }
